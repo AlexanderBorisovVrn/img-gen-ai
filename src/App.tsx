f883b2450f48a2229c74fcf30834e4440cd76434
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2023-03-17 18:52:28 +0300

    feat:add custom logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from "react";
 import { Home, CreatePost } from "./pages";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import {IoMdImages } from 'react-icons/io';
-
+import { Logo } from "./components/Logo";
+/// <reference types="vite-svg-loader" />
 export const App: FC<{}> = () => {
   return (
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 border-b border-b-[#e6ebf4]">
-        <Link to = '/' className="w-14 h-12 my-2 flex justify-center items-center bg-slate-200 rounded">
-          <IoMdImages color="#464be9" size='38'/>
+        <Link to = '/' className="w-14 h-12 my-2 flex justify-center items-center bg-[#6469ff] rounded">
+          <Logo />
         </Link>
         <Link to='/create-post' className="font-medium bg-[#6469ff] text-white tracking-wide rounded-md px-4 py-2">
           Create
